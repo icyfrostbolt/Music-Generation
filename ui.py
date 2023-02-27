f40dc3099ff4b6369555d7a58374210cb2adce00
Ryan <ryclowe@gmail.com>
2023-02-27 11:01:03 -0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,17 +1,23 @@
 import tkinter as tk
+from tkinter import *
 
 window = tk.Tk()
 window.geometry("400x400")
+window.title("Music Generation")
 
-button = tk.Button(
-    text="Add note",
-    width=25,
-    height=0,
-    bg="blue",
-)
+def key_pressed(event):
+    global xcoord
+    if event.char == 'Left':
+        xcoord += 1
+    if event.char == 'Right':
+        xcoord -= 1
+
+window.bind("<Key>", key_pressed)
+
+button = tk.Button(text="Add note",width=25,height=0,bg="cyan")
 button.place(x=200,y=200)
 
-settings = tk.Label(text="General Settings:", background="blue", width="25")
+settings = tk.Label(text="General Settings:", background="cyan", width="25")
 settings.place(x=0,y=200)
 
 volumename = tk.Label(text="Volume:")
@@ -29,11 +35,11 @@ temponameentry = tk.Entry()
 temponame.place(x=0,y=305)
 temponameentry.place(x=0,y=325)
 
-settings = tk.Label(text="Track Settings", background="blue", width="25")
+settings = tk.Label(text="Track Settings", background="cyan", width="25")
 settings.place(x=0,y=345)
 
 track_buttons = []
-xplacement = 0
+xcoord = 0
 for i in range(7):
    buttontemp = tk.Button(text=str(i+1), width=2, height=0, bg="white")
    track_buttons.append(buttontemp)
@@ -58,7 +64,22 @@ timenameentry.place(x=200,y=325)
 
 durationname = tk.Label(text="Duration Name:")
 durationnameentry = tk.Entry()
-durationname.place(x=200,y=345)
-durationnameentry.place(x=200,y=365)
+durationname.place(x=200+xcoord,y=345) 
+durationnameentry.place(x=200+xcoord,y=365)
+
+# mouse wheel has a counter which coordinates to the synthesizer position
+
+def mouse_wheel(event):
+    global count
+    if event.delta == -120:
+        count -= 1 # scroll down
+    if event.delta == 120:
+        count += 1 # scroll up
+    label['text'] = count
+
+count = 0
+window.bind("<MouseWheel>", mouse_wheel)
+label = tk.Label(window, font=('courier', 18, 'bold'), width=10)
+label.pack(padx=40, pady=40)
 
 window.mainloop()
